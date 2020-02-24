ddc7e69f181795346fbebce7c8f35bee28312c40
oskar <oskar.kuiper@gmail.com>
2020-02-24 22:51:59 +0100

    fix warnings

--- src/components/BestExample.tsx
+++ src/components/BestExample.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { inject, observer } from "mobx-react";
 import ExampleStore from "../stores/ExampleStore";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 @inject("exampleStore")
 @observer
@@ -11,15 +10,10 @@ export default class BestExample extends React.Component {
   }
   constructor(props: any) {
     super(props);
-    const { exampleStore } = props;
   }
+
   render() {
     const { exampleStore } = this.stores as { exampleStore: ExampleStore };
-    const theme = createMuiTheme({
-      palette: {
-        type: "dark"
-      }
-    });
     return <div>Some {exampleStore.name} text</div>;
   }
 }
