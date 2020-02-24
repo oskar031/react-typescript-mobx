759f4df395ad9202eacaecbf482f25e88eb23e4f
oskar <oskar.kuiper@gmail.com>
2020-02-24 21:52:19 +0100

    fix error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "mobx-react";
 import stores from "./stores";
